8a33033c0fe78d7a4376d1453b17b60d17e96f48
Shilpa-1304 <101348842+Shilpa-1304@users.noreply.github.com>
2023-02-08 14:22:38 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -6,7 +6,7 @@ const cors = require('cors');
 let server;
 
 // TODO: CRIO_TASK_MODULE_UNDERSTANDING_BASICS - Create Mongo connection and get the express app to listen on config.port
-
+mongoose.set('strictQuery', true);
 mongoose.connect(config.mongoose.url,config.mongoose.options).then(()=>{
     console.log("Connected to MongoDB: "+config.mongoose.url);
     app.listen(8082,()=>{
