81f03176054b9240dc1f8bbbafe5e93844ef32a5
Shilpa-1304 <101348842+Shilpa-1304@users.noreply.github.com>
2023-02-08 21:09:52 +0530

    Update setupRemoteMongo.sh

--- setupRemoteMongo.sh
+++ setupRemoteMongo.sh
@@ -1,1 +1,1 @@
-mongoimport --uri "mongodb+srv://admin:Pa55word@xflix-node.gsby80o.mongodb.net/?retryWrites=true&w=majority" --drop --collection videos --file data.json
+mongoimport --uri "mongodb+srv://admin:Pa55word@xflix-node.gsby80o.mongodb.net/?retryWrites=true&w=majority" --drop --collection videos --file data/export_xflix_videos.json
